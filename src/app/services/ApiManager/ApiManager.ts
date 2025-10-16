d262c862024ea137a7132fb212ca20ddab955a98
Erwin Mario Leon Ramos <79538642+erwin971030@users.noreply.github.com>
2025-10-16 12:15:27 -0400

    ft:order to execution 4

--- src/app/services/ApiManager/ApiManager.ts
+++ src/app/services/ApiManager/ApiManager.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export const ApiManager = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_LOCAL,
-  // baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_PROD,
+  // baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_LOCAL,
+  baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT_PROD,
   // baseURL: "https://ee.renovaenergy.es/api/VicoApi",
   responseType: "json",
   withCredentials: true,
